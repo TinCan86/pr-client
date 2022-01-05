d1362b157a24cf659ee12e80084cf106933b2171
Robert Hansson <roberthanssonr@gmail.com>
2022-01-05 21:43:56 +0100

    add fetch and map

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,21 +5,48 @@ import { Project } from "../models/index";
 import { usePullrequestData } from "../hooks/usePullrequestData";
 
 const LandingView: FunctionComponent = () => {
-  const { data, error } = usePullrequestData();
+  //const { data, error } = usePullrequestData();
+  const [data, setData] = useState<Project[]>([]);
+
+  useEffect(() => {
+    fetch("http://calm-wave-29148.herokuapp.com/pullrequests")
+      .then(response => response.json())
+      .then(data => {
+        setData(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   console.log("Data: ", data)
 
   if (!data) return <p>Loading</p>;
 
-  if (error != null) return <p>error: {error}</p>;
+  // if (error != null) return <p>error: {error}</p>;
 
   // const filtered = JSON.stringify(openPrs, null, 2);
   // console.log(filtered);
 
+  // const filterDevices = devices.filter((device) => {
+  //   if (nameFilter != null) {
+  //     return device.xot_id != null
+  //       ? device.device_name?.toLowerCase().includes(nameFilter)
+  //       : false;
+  //   } else {
+  //     return true;
+  //   }
+  // });
+
   return (
     <React.Fragment>
       <Title>PR Data in ImBox</Title>
-      <p>{data}</p>
+      {data.map((item: Project) => {
+        <div>
+        <p>{item.Title}</p>
+        <p>{item.Status}</p>
+        <p>{item.PullRequestNumber}</p>
+        <p>{item.Url}</p>
+        </div>
+      })}
     </React.Fragment>
   );
 };
