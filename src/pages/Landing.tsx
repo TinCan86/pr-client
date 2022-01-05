681d8d6cab018bc1707db96df84a7e99df3aa673
Robert Hansson <roberthanssonr@gmail.com>
2022-01-05 20:28:40 +0100

    fix fix

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,17 +1,27 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
 import Title from "../components/Title";
 //import { getPullRequestsFromRepos, getGitHubRepos } from "../utils/api";
-import { Project } from "../models/index";
-import { usePullrequestData } from "../hooks/usePullrequestData";
+import axios from 'axios';
 
 const LandingView: FunctionComponent = () => {
-  const { data, error } = usePullrequestData();
+  //const { data, error } = usePullrequestData();
+  const [data, setData] = useState({ hits: [] });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://calm-wave-29148.herokuapp.com/pullrequests',
+      );
+      console.log(result);
+      
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
   console.log("Data: ", data)
 
   if (!data) return <p>Loading</p>;
-
-  if (error != null) return <p>error: {error}</p>;
 
   // const filtered = JSON.stringify(openPrs, null, 2);
   // console.log(filtered);
